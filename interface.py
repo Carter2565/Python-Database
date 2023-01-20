e92efc718e00919693a104904b63e4ac510949cc
Carter2565 <2565tube@gmail.com>
2023-01-19 22:14:04 -0700

    line 17 modify

--- interface.py
+++ interface.py
@@ -14,10 +14,13 @@ while(True):
   # print(f'-{error}-')
   statuscode = False
   for code in settings.error.codes:
+  # if (str(error) in settings.error.codes):
+    # code = response
     # print(f'{error}--{code}')
     if(error == str(code)):
       statuscode = True
       print(f'\n {code} -- {settings.error.codes[code]} \n')
   if(not statuscode):
+  # else:
     print('\n' + str(response.json()) + '\n')   
  
