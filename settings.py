fbfe17b0d9a4a96c4f0d541d56a15a4f999d380d
Carter2565 <2565tube@gmail.com>
2023-02-01 12:57:44 -0700

    Known errors

--- settings.py
+++ settings.py
@@ -6,7 +6,6 @@
 # This project can be used as a template for any python database.
 #
 #-----------------------------------------------------------------------#
-
 class settings:
   class interface:
   # Interface Settings
@@ -24,12 +23,19 @@ class settings:
       202:'Accepted',
       204:'No content to be sent',
       400:'Request error',
-      401:'Unauthorized'
+      401:'Unauthorized',
+      409:'Conflict'
     }
   #-----------------
   class assets:
   # Asset Settings
-    page = 3 #length of asset pages
+    class defalts:
+      class profile:
+        # This is the default pfp. ~~~ I am using a seprate file becouse of the base64 length
+        pfp = open(f"defaults/pfp.base64", "r").read()
+        # This is the default about me message
+        about = "This is user doesn't have an about page yet"
+
   #-----------------
   class file:
   # File settings:
