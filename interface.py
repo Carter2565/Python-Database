cb0fd0c692a976b9dc0fe8dc79f7b1a3cafe340c
Carter2565 <2565tube@gmail.com>
2023-02-02 17:38:45 -0700

    Interface update

--- interface.py
+++ interface.py
@@ -10,24 +10,28 @@
 from settings import settings 
 from database import database
 import requests
-
-while(True):
-  request = input("Enter json: ")
-  print('')
-  if(request == 'exit()'):
-    exit(200)
+from time import sleep
 
 
-  response = requests.post(f"http://{settings.server.ip}:{settings.server.port}", json = request)
-  response = response.json()
-  # print(f'-{error}-')
-  try:
-    error = int(response)
-  except TypeError:
-    error = None
-  finally:
-    if (error in settings.error.codes):
-      print(f'\n {error} -- {settings.error.codes[error]} \n')
-    else:
-      print('\n' + str(response) + '\n')   
- 
+def interface():
+  sleep(1)
+  while(True):
+    request = input("Enter json: ")
+    print('')
+    if(request == 'exit()'):
+      exit(200)
+
+
+    response = requests.post(f"http://{settings.server.ip}:{settings.server.port}", json = request)
+    response = response.json()
+    # print(f'-{error}-')
+    try:
+      error = int(response)
+    except TypeError:
+      error = None
+    finally:
+      if (error in settings.error.codes):
+        print(f'\n {error} -- {settings.error.codes[error]} \n')
+      else:
+        print('\n' + str(response) + '\n')   
+  
