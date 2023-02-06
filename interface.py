f62cfe0996a9450f1ebd3d5732e2d8aa05669f92
Carter2565 <2565tube@gmail.com>
2023-02-05 22:37:19 -0700

    interfase wipe fix

---
+++ interface.py
@@ -0,0 +1,37 @@
+#-----------------------------------------------------------------------#
+#                         --- Python Database ---
+#
+# Contributors: @Carter2565#5594, 
+# This is the current database for PTSO-Exchange.
+# This project can be used as a template for any python database.
+#
+#-----------------------------------------------------------------------#
+
+from settings import settings 
+from database import database
+import requests
+from time import sleep
+
+
+def interface():
+  sleep(1)
+  while(True):
+    request = input("Enter json: ")
+    print('')
+    if(request == 'exit()'):
+      exit(200)
+
+
+    response = requests.post(f"http://{settings.server.ip}:{settings.server.port}", json = request)
+    response = response.json()
+    # print(f'-{error}-')
+    try:
+      error = int(response)
+    except TypeError:
+      error = None
+    finally:
+      if (error in settings.error.codes):
+        print(f'\n {error} -- {settings.error.codes[error]} \n')
+      else:
+        print('\n' + str(response) + '\n')   
+  
